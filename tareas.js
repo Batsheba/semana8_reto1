15f6eff6692fc97d994562bbebe58a6dc8ecb3a0
Batsheba <betsaivette.ms@gmail.com>
2021-10-19 19:12:28 -0500

    update

---
+++ tareas.js
@@ -0,0 +1,10 @@
+let miTarea = document.getElementById("contenido");
+
+let FormTarea = document.getElementById("body");
+
+let tareas = [];
+
+miTarea.addEventListener("span",function(e){
+  
+});
+
